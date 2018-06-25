1c9ba90fea3a33644dbd4e63992fc1885074d1ae
J.J. Ayala <jjayala83@gmail.com>
2018-06-25 18:04:58 -0500

    Add files via upload

---
+++ src/form.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+
+class Form extends React.Component {
+    constructor(props) {
+      // highlight-range{3}
+      super(props);
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.fileInput = React.createRef();
+    }
+    handleSubmit(event) {
+      // highlight-range{4}
+      event.preventDefault();
+      alert(
+        `Selected file - ${
+          this.fileInput.current.files[0].name
+        }`
+      );
+    }
+  
+    render() {
+      // highlight-range{5}
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Upload file:
+            <input type="file" ref={this.fileInput} />
+          </label>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      );
+    }
+  };
+
+  
+    export default Form;
+
