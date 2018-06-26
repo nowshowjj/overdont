9d55f0ba62c8f501adaec0667000db655f48d1cc
Chance Eakin <chance.eakin@gmail.com>
2018-06-25 19:37:02 -0500

    moar fun

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
-import Form from "./components/form.js"
+import React, { Component } from "react";
+import "./App.css";
+import Form from "./components/form2.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-          <Form />
-  
+        <Form />
       </div>
     );
   }
